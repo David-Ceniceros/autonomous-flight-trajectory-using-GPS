456bb088e6886b3612f91eb7ec0b1aa0ac4b226f
David-Ceniceros <84478168+David-Ceniceros@users.noreply.github.com>
2021-06-08 20:45:09 -0600

    Update MasterArduino.ino
    The sole purpose of this program is to verify that the Arduino Uno that is used as a slave is able to obtain data from the GPS and pass the live coordinates to another Arduino Uno through the UART protocol. Only update done was extending the length of the data received from the Arduino Uno slave. This program will no longer be worked on.

--- MasterArduino.ino
+++ MasterArduino.ino
@@ -1,11 +1,12 @@
-char string[10]; //variable to store data
+//Master - This program will become irrelevant as this Arduino Uno will be replace with the flight controller
+char string[44]; //variable to store data
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.readBytes(mystr,5); //Read and store data
+  Serial.readBytes(mystr,24); //Read and store data
   Serial.println(mystr); 
   delay(1000);
 }
