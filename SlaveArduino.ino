f8a44154f6814355cd030090448a5a2997dde51e
David-Ceniceros <84478168+David-Ceniceros@users.noreply.github.com>
2021-06-08 20:37:33 -0600

    Update SlaveArduino.ino
    This is the result of integrating the code from Example-TinyGPS.ino (https://github.com/David-Ceniceros/autonomous-flight-trajectory-using-GPS/blob/main/Example-TinyGPS.ino) with that of SlaveArduino.ino. This code is able to obtain GPS coordinates from the GPS module connected to the Arduino Uno through the I2C protocol and is able to transmit the live data to another Arduino connected through the UART protocol. I have modified SlaveArduino.ino to also read data up to the sixth decimal place for better accuracy. Future modifications of this program will include programming LEDs that will light up whenever the GPS is within certain coordinates.

--- SlaveArduino.ino
+++ SlaveArduino.ino
@@ -1,10 +1,56 @@
-char string[5] = "Hello"; //String data
+//Slave - eventually to be programmed to work with flight controller
+#include <SparkFun_I2C_GPS_Arduino_Library.h> //Use Library Manager or download here: https://github.com/sparkfun/SparkFun_I2C_GPS_Arduino_Library
+I2CGPS myI2CGPS; //Hook object to the library
+
+#include <TinyGPS++.h> //From: https://github.com/mikalhart/TinyGPSPlus
+TinyGPSPlus gps; //Declare gps object
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("GTOP Read Example");
+
+  if (myI2CGPS.begin() == false)
+  {
+    Serial.println("Module failed to respond. Please check wiring.");
+    while (1); //Freeze!
+  }
+  Serial.println("GPS module found!");
 }
 
 void loop() {
-  Serial.write(string,5); //Write the serial data
-  delay(1000);
+  while (myI2CGPS.available()) //available() returns the number of new bytes available from the GPS module
+  {
+    gps.encode(myI2CGPS.read()); //Feed the GPS parser
+  }
+
+  if (gps.time.isUpdated()) //Check to see if new GPS info is available
+  {
+    displayInfo(); //This is the data that is to be displayed
+  }
+}
+
+void displayInfo()
+{
+  //Read and print data to  microcontroller/Flight controller
+  if (gps.time.isValid())
+  {
+    
+  }
+  else
+  {
+    Serial.println(F("Time not yet valid"));
+  }
+
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6); //Display latitude
+    Serial.print(F(", "));
+    Serial.print(gps.location.lng(), 6); //Display longitude
+    Serial.println();
+    delay(1000);
+  }
+  else
+  {
+    Serial.println(F("Location not yet valid"));
+  }
 }
