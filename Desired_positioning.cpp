9a8bd82751b06421505e0c9a614f38d9d862cdd9
David-Ceniceros <84478168+David-Ceniceros@users.noreply.github.com>
2021-06-30 11:19:21 -0600

    Update Desired_positioning.cpp
    This program is to be used on the Cygnus IDE. This code will display messages on the monitor to demonstrate the autonomously trajectory of the plutoX drone. The trajectory will be in a shape of a square. A timer will indicate when the drone is to move to a location. This is currently a work in progress as I will need to incorporate the GPS to the plutoX flight controller and will replace the messages with commands.

--- Desired_positioning.cpp
+++ Desired_positioning.cpp
@@ -1,82 +1,48 @@
-// Do not remove the include below
+// this is to program drone to move from one position to another
 #include "PlutoPilot.h"
-#include "Control.h"
+#include "Utils.h"
 #include "User.h"
 
-int32_t initial_x_Axis;
-int32_t initial_y_Axis;
-int32_t moving_x_Axis;
-int32_t moving_y_Axis;
-int32_t future_x_Axis = 10; //place holder
-int32_t future_y_Axis = 10; //place holder
+int timer = 0;
 
 void plutoInit()
 {
-
 }
 
 void onLoopStart()
 {
-	//obtains current position
-	initial_x_Axis = DesiredPosition.get(X);
-	initial_y_Axis = DesiredPosition.get(Y);
-
-	//sets current positions to zero
-	DesiredPosition.set(X,0);
-	DesiredPosition.set(Y,0);
-
 }
 
 void plutoLoop()
 {
-	// allows the drone to autonomously fly toward first position
-	if (initial_x_Axis <= 0 && initial_y_Axis <= 0) {
-		initial_y_Axis = moving_y_Axis;
-		moving_y_Axis++;
-		RcCommand.set(RC_PITCH, 1400);
+	timer++; // 100 = 10 seconds
+	if (timer < 30) // forward for 3 secs
+	{ 
+		Monitor.println("forward");
+	}
+	if (timer > 30 && timer < 45) // stops for 1.5 secs
+	{
+		Monitor.println("stop");
+	}
+	if (timer > 46 && timer < 76) // backwards for 3 secs
+	{
+		Monitor.println("backward");
+	}
+	if (timer > 77 && timer < 92) // stops for 1.5 secs
+	{
+		Monitor.println("stop again");
+	}
+	if (timer > 93 && timer < 123) // forward again for 3 secs
+	{
+		Monitor.println("forward again");
+	}
+	if (timer > 123) // land drone
+	{
+		Monitor.println("land drone");
 	}
 
-	// when drone gets to first position, it stops
-	if (moving_y_Axis = future_y_Axis) {
-		RcCommand.set(RC_PITCH, 1500);
-	}
-
-	// allows the drone to autonomously fly toward second position
-	if (initial_x_Axis <= 0 && moving_y_Axis >= 10) {
-		initial_x_Axis = moving_x_Axis;
-		moving_x_Axis++;
-		RcCommand.set(RC_ROLL, 1400);
-	}
-
-	// when drone gets to second position, it stops
-	if (moving_x_Axis = future_x_Axis) {
-		RcCommand.set(RC_ROLL, 1500);
-	}
-
-	// allows the drone to autonomously fly toward third position
-	if (moving_x_Axis >= 10 && moving_y_Axis >= 10) {
-		moving_y_Axis--;
-		RcCommand.set(RC_PITCH, 1600);
-	}
-
-	// when drone gets to third position, it stops
-	if (moving_y_Axis = initial_y_Axis) {
-		RcCommand.set(RC_PITCH, 1500);
-	}
-
-	// allows the drone to autonomously fly toward initial position
-	if (initial_x_Axis >= 10 && initial_y_Axis <= 0) {
-		moving_y_Axis--;
-		RcCommand.set(RC_ROLL, 1600);
-	}
-
-	// when drone gets to initial position, it stops
-	if (moving_x_Axis = initial_x_Axis) {
-		RcCommand.set(RC_ROLL, 1500);
-	}
 }
 
 void onLoopFinish()
 {
-
 }
